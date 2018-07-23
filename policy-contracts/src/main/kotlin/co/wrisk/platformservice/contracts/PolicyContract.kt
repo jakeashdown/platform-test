2661dc93df98941ef809a36bb0b0037e3f387f7f
Richard Walters <richard@wrisk.co>
2018-07-23 14:09:34 +0100

    Adds missing price property to PolicyContract

--- policy-contracts/src/main/kotlin/co/wrisk/platformservice/contracts/PolicyContract.kt
+++ policy-contracts/src/main/kotlin/co/wrisk/platformservice/contracts/PolicyContract.kt
@@ -3,6 +3,7 @@ package co.wrisk.platformservice.contracts
 import java.math.BigDecimal
 
 data class PolicyContract(
+        val price: BigDecimal,
         val highestExcess: BigDecimal,
         val totalSumInsured: BigDecimal,
         val sections: List<SectionContract>
